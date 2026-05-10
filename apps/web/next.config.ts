32fd6ea210251e46e9d8a9fa7ff1f96bea6d6ed9
Marwanbz <kaka.mer1998@gmail.com>
2026-05-10 23:43:32 +0300

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@nextjs-server-demo/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
